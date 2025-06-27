ef1238f205648dd847982ace02599f26e703f637
developersahil68 <sahil858700@gmail.com>
2025-06-27 10:28:05 +0530

    Initialized Frontend

---
+++ mytribehub-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-4 rounded cursor-pointer">
+      Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
